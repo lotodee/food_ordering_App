59450a0deba61e3d4bc9306136a318d9ef122a77
lotodee <olamidotun.loto@decagon.dev>
2024-04-08 15:42:40 +0100

    productlistcomponent

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,38 +1,10 @@
-import { StyleSheet,Text,View,Image } from 'react-native';
-
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import Colors from '@/src/constants/Colors';
-import products from '@/assets/data/products';
-
-const product  = products[0]
-export default function TabOneScreen() {
+import products from "@/assets/data/products";
+import ProductListItem from "@/src/components/ProductListItem";
+import { View } from 'react-native';
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{uri:product.image}} style={styles.image }/>
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem product={products[4]}/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-container:{
-backgroundColor:'white',
-padding:10,
-borderRadius:20
-},
-  image:{
-    width:'100%',
-    aspectRatio:1
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginVertical:10,
-  },
-
-  price:{
-     color:Colors.light.tint,
-     fontWeight:'bold'
-  }
-});
